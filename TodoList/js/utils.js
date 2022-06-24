3c819d1f71d2b4904e79eda5eea67d18435efc29
SilasEduardo <silasandrade94@gmail.com>
2022-06-24 15:01:55 -0300

    curso firebase

---
+++ TodoList/js/utils.js
@@ -0,0 +1,38 @@
+const authForm = document.getElementById('authForm')
+const authFormTitle = document.getElementById('authFormTitle')
+
+
+const register = document.getElementById('register')
+const access = document.getElementById('access')
+
+
+// alterar formulario de autenticação para o cadastro de novas contas
+
+function toggleToRegister(){
+    authForm.submitAuthForm.innerHTML = 'Cadastrar Conta';
+    authFormTitle.innerHTML = 'Insira seus dados para cadastrar'
+
+
+    hideItem(register)
+    showItem(access)
+}
+//alterar novas contas
+function toggleToAccess(){
+    authForm.submitAuthForm.innerHTML = 'Acessar';
+    authFormTitle.innerHTML = 'Acesse sua conta para continuar'
+
+
+    hideItem(access)
+    showItem(register)
+}
+
+
+//simplifica elemento da pagina
+function showItem(element){
+    element.style.display = 'block'
+}
+
+function hideItem(element){
+    element.style.display = 'none'
+}
+
